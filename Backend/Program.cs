e9a8b621405582c294ddc24a1710f65e3a409f57
alastorj <alastorj@student.21-school.ru>
2026-03-09 17:04:10 +0500

    Enable Swagger in production

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -52,11 +52,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors("AllowAll");
 app.UseAuthentication();
